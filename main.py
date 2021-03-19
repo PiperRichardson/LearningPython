7ef30b39ad65e2aa4f0b81380be3d9678fc401d2
PiperRichardson <817420@mystma.org>
2021-03-19 15:27:39 +0000

    mad lib

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-print("Zootopia")
-print("Incredibles")
-print("Maze Runner")
-print("Infinity War")
-print("End Game")
+adjectiveOne = purple
+
+
+print ("In the early hours of Wednesday, Febuary 10th, 1880 the town was jolted awake by a" + adjectiveOne + "sound. The sound would turn out to be that of the safe at the First National Bank being")
