8e789bac7f58fe72c3d52697e91191a036a126eb
PiperRichardson <817420@mystma.org>
2021-03-22 15:06:47 +0000

    Mad Libs

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 adjectiveOne = purple
-
-
-print ("In the early hours of Wednesday, Febuary 10th, 1880 the town was jolted awake by a" + adjectiveOne + "sound. The sound would turn out to be that of the safe at the First National Bank being")
+adverbOne = accidentally 
+verbOne = jumping
+timeOfNight = 1:23 AM
+name = Jimmy
+typeOfBuilding = Walmart
+number = 32
+place = park
+numberTwo = 68
+adverbTwo = sleepily 
+noun = table
+nounTwo = pandas
+print ("In the early hours of Wednesday, Febuary 10th, 1880 the town was jolted awake by a" + adjectiveOne + "sound. The sound would turn out to be that of the safe at the First National Bank being" + adverbOne + verbOne + "! At the time, the bank was housed in what we know today as National Hall."
+"At" + timeOfNight + ", the downtown night watchman," + name + ", was attacked by a group of men and taken to wait behind a" + typeOfBuilding + "near the bank. after what he thought to be about" + number + "hours, he was moved to a" + place + "inside the bank. After" + numberTwo + "hours work, the men decided to" + adverbTwo + "close the vault door. Sadly, they used far too much" + noun + "!"
+"the explosion caused them to panic and grab as much" + nounTwo + "as they could, and make a run for the train."
