b4b648fdc7d3606224b9608f0e646188f495de1b
PiperRichardson <817420@mystma.org>
2021-03-23 15:19:45 +0000

    Functions Practice

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-adjectiveOne = input("Input an adjective\n")
-adverbOne = input("Input adverb\n")
-verbOne = input("Input verb\n")
-timeOfNight = input("Input time of night\n")
-name = input("Input name \n")
-typeOfBuilding = input("Input a type of building\n")
-number = input("Input a number\n")
-place = input("Input a place\n")
-numberTwo = input("Input a number\n")
-adverbTwo = input("Input an adverb\n") 
-noun = input("Input a noun\n")
-nounTwo = input("Input a noun\n")
-print ("In the early hours of Wednesday, Febuary 10th, 1880 the town was jolted awake by a" + adjectiveOne + "sound. The sound would turn out to be that of the safe at the First National Bank being" + adverbOne + verbOne + "! At the time, the bank was housed in what we know today as National Hall."
-"At" + timeOfNight + ", the downtown night watchman," + name + ", was attacked by a group of men and taken to wait behind a" + typeOfBuilding + "near the bank. after what he thought to be about" + number + "hours, he was moved to a" + place + "inside the bank. After" + numberTwo + "hours work, the men decided to" + adverbTwo + "close the vault door. Sadly, they used far too much" + noun + "!"
-"the explosion caused them to panic and grab as much" + nounTwo + "as they could, and make a run for the train.")
+def addTwoNumbers(x, y):
+    return x + y
+
+print(addTwoNumbers(5, 7))
+
+
+
